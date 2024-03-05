828e8694ff726366f71a9c0405d81298ea93646b
Leonardo Senff <112514561+leosenff@users.noreply.github.com>
2024-03-04 21:02:04 -0300

    Add some extra buttoms and a display

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const screen = document.querySelector(".display");
+screen.textContent = "0";
+
 let n1 = 4;
 let n2 = 2;
 let operand = '+';
@@ -20,14 +23,15 @@ function div(n1,n2){
 
 function operate(operand){
     if (operand == "+")
-    return add(n1,n2);
+    return screen.textContent = add(n1,n2);
 
     if (operand =="-")
-    return sub(n1,n2);
+    return screen.textContent = sub(n1,n2);
 
     if (operand == "*")
-    return multi(n1,n2);
+    return screen.textContent = multi(n1,n2);
 
     if (operand == "/")
-    return div(n1,n2);
+    return screen.textContent = div(n1,n2);
 }
+
