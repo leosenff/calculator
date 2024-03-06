b3f6e414e23d44e370d8a05ce2d5aa79da12d2b1
Leonardo Senff <112514561+leosenff@users.noreply.github.com>
2024-03-06 18:15:06 -0300

    Re-doing from scratch

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
-const screen = document.querySelector(".display");
-screen.textContent = "0";
+const display = document.querySelector(".display");
+const btnNumbers = document.querySelectorAll(".numbers");
+const operateBtn = document.querySelectorAll(".operator");
+display.textContent = 0;
 
-let n1 = 4;
-let n2 = 2;
-let operand = '+';
+let num1;
+let num2;
+let operator;
 
-function add(n1,n2){
-    return n1 + n2;
+
+function add(num1, num2){
+    return num1 + num2;
 }
 
-function sub(n1,n2){
-    return  n1 - n2;
+function sub(num1, num2){
+    return num1 - num2;
 }
 
-function multi(n1,n2){
-    return n1 * n2;
+function mult(num1, num2){
+    return num1 * num2;
 }
 
-function div(n1,n2){
-    return n1 / n2;
+function div(num1, num2){
+    return num1 / num2;
 }
 
-function operate(operand){
-    if (operand == "+")
-    return screen.textContent = add(n1,n2);
+function operate(num1, num2, operate){
+    if (operate == "+")
+    result = add(num1, num2);
 
-    if (operand =="-")
-    return screen.textContent = sub(n1,n2);
+    if (operate == "-")
+    result = sub(num1, num2);
 
-    if (operand == "*")
-    return screen.textContent = multi(n1,n2);
+    if (operate == "*")
+    result = mult(num1, num2);
 
-    if (operand == "/")
-    return screen.textContent = div(n1,n2);
+    if (operate == "/")
+    result = div(num1, num2);
 }
 
+for (let btn of btnNumbers){
+    btn.addEventListener('click', () => {
+        num1 = btn.value;
+        display.textContent = btn.value;
+    });
+}
