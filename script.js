46ce385f5a935aa504de1c3b8fb568fb7b883ae3
Leonardo Senff <112514561+leosenff@users.noreply.github.com>
2024-03-07 14:43:37 -0300

    Add new buttons and EventListenrs

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const display = document.querySelector(".display");
 const btnNumbers = document.querySelectorAll(".numbers");
-const operateBtn = document.querySelectorAll(".operator");
+const operatorBtns = document.querySelectorAll(".operator");
+const equalsBtn = document.querySelector(".equals");
 display.textContent = 0;
 
-let num1;
-let num2;
-let operator;
-
+let num1 = "";
+let num2 = "";
+let operator = "";
+let equals = "";
+let result = 0;
 
 function add(num1, num2){
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function sub(num1, num2){
@@ -25,22 +27,47 @@ function div(num1, num2){
 }
 
 function operate(num1, num2, operate){
-    if (operate == "+")
+    if (operate == "add"){
     result = add(num1, num2);
-
-    if (operate == "-")
+    display.textContent = result;
+}
+    if (operate == "sub")
     result = sub(num1, num2);
 
-    if (operate == "*")
+    if (operate == "mult")
     result = mult(num1, num2);
 
-    if (operate == "/")
+    if (operate == "div")
     result = div(num1, num2);
 }
 
 for (let btn of btnNumbers){
     btn.addEventListener('click', () => {
-        num1 = btn.value;
-        display.textContent = btn.value;
+        if (num1 == "" || operator == ""){
+            num1 += btn.value;
+            display.textContent = num1; 
+        } else if (equals !== "equals"){
+            num2 += btn.value;
+            display.textContent = num2;
+         }
     });
 }
+
+for (let btnOperator of operatorBtns) {
+    btnOperator.addEventListener('click', () => {
+        operator = btnOperator.value;
+        if (operator == "+"){
+            display.textContent = operator;
+            return operator = "add";
+        } if (operator == "-"){
+            display.textContent = operator;
+            return operator = "add";
+        }
+        
+    });
+}
+
+equalsBtn.addEventListener('click', () => {
+    equals = "equals";
+    operate(num1, num2, operator);
+});
